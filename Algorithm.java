6dbce7821c7385947f792f2f0e06968947922b73
Ansor Kasimov <ansor@ansors-imac.lan>
2021-09-25 08:36:37 -0400

    Clean up code

--- Algorithm.java
+++ Algorithm.java
@@ -9,18 +9,18 @@ public class Algorithm {
 		int n = input.nextInt();
 		input.close();
 		boolean p[] = new boolean[n];
-		for(int i = 0; i < n; i++) 
+		for (int i = 0; i < n; i++) 
 			p[i] = true;
 		String output = "";
 		
-		for(int i = 2; i < Math.sqrt(n); i++) 
-			if(p[i] == true) 
-				for(int j = (i*i); j<n; j+=i)
+		for (int i = 2; i < Math.sqrt(n); i++) 
+			if (p[i] == true) 
+				for (int j = (i * i); j < n; j += i)
 					p[j] = false;
 			
 		System.out.println("\n" + "Prime Numbers in the range 2 to " + n + ": ");
-		for(int i = 2; i < n; i++) 
-			if(p[i] == true)
+		for (int i = 2; i < n; i++) 
+			if (p[i] == true)
 				output += i + ", ";
 		
 		output = output.replaceAll(", $", "");
